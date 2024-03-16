1a115345fe2b8193c9750be2ac7632054461d865
Tim Chandler <tim@code-distortion.net>
2024-03-17 01:06:29 +1100

    Updated PHP container aliases

--- build/php/profile.d/phpunit_aliases.sh
+++ build/php/profile.d/phpunit_aliases.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
 alias phpunit='./vendor/bin/phpunit --stop-on-error --stop-on-failure --display-deprecations --display-notices --display-warnings --exclude-group=skip,quarantined'
-alias phpunit-skip='./vendor/bin/phpunit --stop-on-error --stop-on-failure --display-deprecations --display-notices --display-warnings --group=skip'
-alias phpunit-quarantined='./vendor/bin/phpunit --stop-on-error --stop-on-failure --display-deprecations --display-notices --display-warnings --group=quarantined'
 alias t='phpunit'
-alias infection='./vendor/bin/infection --threads=max --show-mutations'
+alias tf='phpunit --filter'
+
+alias phpunit-skip='./vendor/bin/phpunit --stop-on-error --stop-on-failure --display-deprecations --display-notices --display-warnings --group=skip'
+alias s='phpunit-skip'
+
+alias phpunit-quarantined='./vendor/bin/phpunit --stop-on-error --stop-on-failure --display-deprecations --display-notices --display-warnings --group=quarantined'
+alias q='phpunit-quarantined'
+
+alias infection='vendor/bin/infection --threads=max --show-mutations --test-framework-options="--exclude-group=skip"'
+alias i='infection'
