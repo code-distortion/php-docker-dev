098e9f7f956fbc8ebb2242e9cfeedcbeeaa7cc1f
Tim Chandler <tim@code-distortion.net>
2025-01-28 00:57:24 +1100

    Added aliases for older versions of PHPUnit

--- build/php/profile.d/phpunit_aliases.sh
+++ build/php/profile.d/phpunit_aliases.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-#alias phpunit='./vendor/bin/phpunit --stop-on-error --stop-on-failure --display-deprecations --display-phpunit-deprecations --display-notices --display-warnings --exclude-group=skip,quarantined'
+
+
+# aliases for the current version of PHPUnit
 alias phpunit='./vendor/bin/phpunit --stop-on-error --stop-on-failure --display-deprecations --display-phpunit-deprecations --display-notices --display-warnings --exclude-group=skip --exclude-group=quarantined'
 alias t='phpunit'
 alias tf='./vendor/bin/phpunit --stop-on-error --stop-on-failure --display-deprecations --display-phpunit-deprecations --display-notices --display-warnings --filter'
@@ -13,3 +15,19 @@ alias q='phpunit-quarantined'
 
 alias infection='vendor/bin/infection --threads=max --show-mutations --test-framework-options="--exclude-group=skip"'
 alias i='infection'
+
+
+
+# aliases for older versions of PHPUnit
+alias old-phpunit='./vendor/bin/phpunit --stop-on-error --stop-on-failure --exclude-group=skip,quarantined'
+alias ot='old-phpunit'
+alias otf='./vendor/bin/phpunit --stop-on-error --stop-on-failure --filter'
+
+alias old-phpunit-skip='./vendor/bin/phpunit --stop-on-error --stop-on-failure --group=skip'
+alias os='old-phpunit-skip'
+
+alias old-phpunit-quarantined='./vendor/bin/phpunit --stop-on-error --stop-on-failure --group=quarantined'
+alias oq='old-phpunit-quarantined'
+
+alias old-infection='vendor/bin/infection --threads=max --show-mutations --test-framework-options="--exclude-group=skip"'
+alias oi='old-infection'
