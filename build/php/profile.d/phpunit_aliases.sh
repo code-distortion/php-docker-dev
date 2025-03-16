67f46188575dfa9fee599adf7de1d1cb8cd55b02
Tim Chandler <tim@code-distortion.net>
2025-03-16 21:51:52 +1100

    Updated PHPUnit aliases

--- build/php/profile.d/phpunit_aliases.sh
+++ build/php/profile.d/phpunit_aliases.sh
@@ -3,31 +3,25 @@
 
 
 # aliases for the current version of PHPUnit
-alias phpunit='./vendor/bin/phpunit --stop-on-error --stop-on-failure --display-deprecations --display-phpunit-deprecations --display-notices --display-warnings --exclude-group=skip --exclude-group=quarantined'
+alias phpunit='./vendor/bin/phpunit --stop-on-error --stop-on-failure --display-deprecations --display-phpunit-deprecations --display-notices --display-warnings --exclude-group=skip --exclude-group=quarantined --exclude-group=3rdParty'
+alias phpunit-skip='./vendor/bin/phpunit --stop-on-error --stop-on-failure --display-deprecations --display-phpunit-deprecations --display-notices --display-warnings --group=skip'
+alias phpunit-quarantined='./vendor/bin/phpunit --stop-on-error --stop-on-failure --display-deprecations --display-phpunit-deprecations --display-notices --display-warnings --group=quarantined'
+
 alias t='phpunit'
+alias ta='./vendor/bin/phpunit --stop-on-error --stop-on-failure --display-deprecations --display-phpunit-deprecations --display-notices --display-warnings --exclude-group=skip --exclude-group=quarantined'
+alias t3='./vendor/bin/phpunit --stop-on-error --stop-on-failure --display-deprecations --display-phpunit-deprecations --display-notices --display-warnings --group=3rdParty'
 alias tf='./vendor/bin/phpunit --stop-on-error --stop-on-failure --display-deprecations --display-phpunit-deprecations --display-notices --display-warnings --filter'
 
-alias phpunit-skip='./vendor/bin/phpunit --stop-on-error --stop-on-failure --display-deprecations --display-phpunit-deprecations --display-notices --display-warnings --group=skip'
-alias s='phpunit-skip'
-
-alias phpunit-quarantined='./vendor/bin/phpunit --stop-on-error --stop-on-failure --display-deprecations --display-phpunit-deprecations --display-notices --display-warnings --group=quarantined'
-alias q='phpunit-quarantined'
-
-alias infection='vendor/bin/infection --threads=max --show-mutations --test-framework-options="--exclude-group=skip"'
-alias i='infection'
+alias i='vendor/bin/infection --threads=max --show-mutations --test-framework-options="--exclude-group=skip"'
 
 
 
 # aliases for older versions of PHPUnit
-alias old-phpunit='./vendor/bin/phpunit --stop-on-error --stop-on-failure --exclude-group=skip,quarantined'
+alias old-phpunit='./vendor/bin/phpunit --stop-on-error --stop-on-failure --display-deprecations --display-phpunit-deprecations --display-notices --display-warnings --exclude-group=skip,quarantined,3rdParty'
+alias old-phpunit-skip='./vendor/bin/phpunit --stop-on-error --stop-on-failure --display-deprecations --display-phpunit-deprecations --display-notices --display-warnings --group=skip'
+alias old-phpunit-quarantined='./vendor/bin/phpunit --stop-on-error --stop-on-failure --display-deprecations --display-phpunit-deprecations --display-notices --display-warnings --group=quarantined'
+
 alias ot='old-phpunit'
-alias otf='./vendor/bin/phpunit --stop-on-error --stop-on-failure --filter'
-
-alias old-phpunit-skip='./vendor/bin/phpunit --stop-on-error --stop-on-failure --group=skip'
-alias os='old-phpunit-skip'
-
-alias old-phpunit-quarantined='./vendor/bin/phpunit --stop-on-error --stop-on-failure --group=quarantined'
-alias oq='old-phpunit-quarantined'
-
-alias old-infection='vendor/bin/infection --threads=max --show-mutations --test-framework-options="--exclude-group=skip"'
-alias oi='old-infection'
+alias ota='./vendor/bin/phpunit --stop-on-error --stop-on-failure --display-deprecations --display-phpunit-deprecations --display-notices --display-warnings --exclude-group=skip,quarantined'
+alias ot3='./vendor/bin/phpunit --stop-on-error --stop-on-failure --display-deprecations --display-phpunit-deprecations --display-notices --display-warnings --group=3rdParty'
+alias otf='./vendor/bin/phpunit --stop-on-error --stop-on-failure --display-deprecations --display-phpunit-deprecations --display-notices --display-warnings --filter'
